1b54782fb850eb14a41696e5f07d579ceb9c45e1
unknown <khushishukla0502@gmail.com>
2021-09-25 13:04:17 +0530

    Adding card and design

--- src/components/ListDetails/ListDetails.js
+++ src/components/ListDetails/ListDetails.js
@@ -1,44 +1,92 @@
 import React, { useState, useEffect} from 'react'
 import './ListDetails.css'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+
+const mapping=(borders)=>{
+    borders.map((border)=>{
+        <div>{border}</div>
+    })
+}
 
 function ListDetails() {
-
-    const [nameVal, setNameVal]=useState();
     const [inputval, setInputVal]=useState([]);
 
-    useEffect(() => {
+    const getCountries=()=>{
         // GET request using fetch inside useEffect React hook
-        fetch('https://restcountries.com/v3/region/asia')
-            .then(response => response.json())
-            .then(data => setInputVal(data));
+            fetch('https://restcountries.com/v3/region/asia')
+                .then(response => response.json())
+                .then(data => setInputVal(data));
+        
+    }        
     
+
+    useEffect(() => {
+      getCountries();
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
     }, []);
 
+    const refreshCountries=()=>{
+        setInputVal([]);
+            setTimeout(() => {
+              getCountries();
+            }, 1000);
+    }
+
     const showName=(countries)=>{
-        
+        //displaying the country in a card
+        if(!countries.length){
+            return(
+            <Box sx={{ width: '100%' }}>
+                <LinearProgress />
+            </Box>
+            );
+        }
+        else
         return (
             countries.map(country => 
-                <div>
-                    <p>
-                        <p>Name: {country.name.common} <br/>Capital: {country.capital} <br/>
-                        Region: {country.region} <br/>Sub-region: {country.subregion} <br/>
-                        Population: {country.population} <br/>
-                        <img className="flag" alt={country.flag} src={country.flags[0]} />
-                        <br/></p>
-                    </p>
-                </div>)
+                <Card key={country.ccn3} className="card" sx={{ maxWidth: 345 }} >
+                    <CardMedia
+                       className="flag"
+                       component="img"
+                        image={country.flags[0]}
+                        alt={country.flag}
+                     />
+
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {country.name.common}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Capital: {country.capital} <br/>
+                            Region: {country.region} <br/>Sub-region: {country.subregion} <br/>
+                            Population: {country.population} <br/>
+                            Borders: 
+                                <li>{country.borders}</li>
+                            
+                        </Typography>
+                    </CardContent>
+                </Card>
+                )
         );
         
     }
 
     return (
-        <div class="list-details">
-            {/* {inputval.map(h => <div>{h.name.common}</div>)} */}
-          {showName(inputval)}
-            {/* <button onClick={()=>setNameVal('')}>Click</button> */}
-            {nameVal}
-           
+        <div>
+            <div>
+                {/* this button will refresh the data from the api */}
+                <Button  variant="contained" onClick={refreshCountries}>Refresh</Button>
+            </div>
+            <div className="list-details">
+            {showName(inputval)}          
+            
+            </div>
         </div>
     )
 }
